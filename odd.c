402f7aa4e235e3a3043a1e2c42cf8dbf9c49120f
agalyapps <37831308+agalyapps@users.noreply.github.com>
2018-03-27 16:13:38 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int r;
+printf("Enter the value of r:");
+scanf("%d",&r);
+if(r%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+	return 0;
+}
