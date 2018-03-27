4dcf3552d5f170d567d1f244c6240caa2193390f
agalyapps <37831308+agalyapps@users.noreply.github.com>
2018-03-27 16:24:46 +0530

    Create leap or not.c

---
+++ leap or not.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int r;
+printf("Enter the value of r:");
+scanf("%d",&r);
+if(r%4==0)
+{
+printf("leap year");
+}
+else
+{
+printf("not a leap year");
+}
+	return 0;
+}
